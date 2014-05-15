bfca4e38f2ecc59d753f2aa42c8c7553aecd68cd
Stephen Costello <stephen@mammoth.tv>
2014-05-15 17:12:54 +0100

    Random changes

--- page.php
+++ page.php
@@ -1,15 +1,19 @@
 <?php Starkers_Utilities::get_template_parts( array( 'header' ) ); ?>
 
 
-  <div class="main-content">
-    <div class="container">
+  <section id="content">
 
-      <article class="primary">Neat is an open source semantic grid framework
-      built on top of Sass and Bourbon&hellip;</article>
-      <aside class="secondary">Discover more at neat.bourbon.io</aside>
+    <div class="main-content">
+      <div class="container">
 
+        <article class="primary">Neat is an open source semantic grid framework
+        built on top of Sass and Bourbon&hellip;</article>
+        <aside class="secondary">Discover more at neat.bourbon.io</aside>
+
+      </div>
     </div>
-  </div>
+
+  </section>
 
 
 <?php Starkers_Utilities::get_template_parts( array( 'footer') ); ?>
