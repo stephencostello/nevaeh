0dd7deae6e04d818af66c0fd47113e8584971bfd
Stephen Costello <stephen@mammoth.tv>
2014-05-15 14:36:34 +0100

    CSS now calls Neat

--- page.php
+++ page.php
@@ -1,30 +1,12 @@
 <?php Starkers_Utilities::get_template_parts( array( 'header' ) ); ?>
 
 
-  <div id="content">
+  <div class="main-content">
     <div class="container">
 
-      <article class="co10 coCenter">
-        <!-- <img src="http://lorempizza.com/856/500" class="mb40" /> -->
-        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-          <h1><?php the_title(); ?></h1>
-         <?php the_content(); ?>
-        <?php endwhile; ?>
-      </article>
-
-      <!-- <aside class="co3">
-          sidebar
-      </aside> -->
-
-      <div class="co10 coCenter textCenter">
-        <ul class="subPages">
-          <li><a href="#">Home</a></li>
-          <li><a href="#" class="current">About</a></li>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Hello</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </div>
+      <article class="primary">Neat is an open source semantic grid framework
+      built on top of Sass and Bourbon&hellip;</article>
+      <aside class="secondary">Discover more at neat.bourbon.io</aside>
 
     </div>
   </div>
