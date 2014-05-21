d407e61562a8b04cefab999d82d3d943bb87b5c9
Stephen Costello <stephen@mammoth.tv>
2014-05-21 17:14:39 +0100

    Nav fixed further

--- page.php
+++ page.php
@@ -4,14 +4,22 @@
   <div id="content">
     <div class="container">
 
+      <div class="co12">
+        <img src="http://lorempixel.com/766/400/" class="floatRight" />
+      </div>
+
+      <hr class="clear40" />
+
       <?php Starkers_Utilities::get_template_parts( array( 'parts/sidebar') ); ?>
 
-      <article class="co9 mb80">
+      <article class="co9">
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
           <h1><?php the_title(); ?></h1>
           <?php the_content(); ?>
         <?php endwhile; ?>
       </article>
+
+      <hr class="clear80" />
 
       <ul class="gce-list">
         <li>
